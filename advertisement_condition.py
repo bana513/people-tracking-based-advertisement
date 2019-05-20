bd5a435345f199819c389d751ef0aad7e0ae24b1
Bana <bial.bence@sch.bme.hu>
2019-05-20 12:06:22 +0200

    advertisement conditions removed

--- advertisement_condition.py
+++ advertisement_condition.py
@@ -1,4 +1,2 @@
 def get_advertisement(total_people, moving_in, moving_out, moving_left, moving_right):
-    if total_people>=4:
-        return 'logo.PNG', 'total_people>=4'
     return None, 'No satisfactory condition'
